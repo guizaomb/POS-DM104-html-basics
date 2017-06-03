23bef978a026cc9c6ca241d85072914c2b6435f8
Guilherme Ferreira <guizaomb@gmail.com>
2017-06-03 14:30:24 -0300

    Added JS

---
+++ first-html/index.js
@@ -0,0 +1,15 @@
+   console.log('Hi there!')
+
+/*   function sleep (milliSeconds) {
+        let startTime = Date.now()
+        while(Date.now() < startTime + milliSeconds);
+   }
+
+   sleep(2000);
+
+   console.log('Hi there after sleep')
+*/
+
+   for(let i; i <10; i++) {
+       console.log('Hi number ' +i)
+   }
